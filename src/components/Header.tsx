6d8caba41ae74b633cdf66e723101777a2a7e87b
Anas A. <97006213+Flekzishot@users.noreply.github.com>
2025-10-15 02:28:58 +0100

    Add files via upload

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,15 +7,15 @@ export default function Header() {
 
       <div className="container mx-auto px-1 py-6 flex items-center justify-between">
 
-        <Link to="/" className="text-xl font-semibold text-black-900  dark:border-gray-900 dark:text-white">test</Link>
+        <Link to="/" className="text-xl font-semibold text-black-900  dark:border-gray-900 dark:text-white">Anas A.</Link>
 
         <nav className="flex items-center space-x-4">
 
-          <Link to="/about" className="text-gray-700 dark:text-gray-200 hover:text-gray-200 dark:hover:text-white">About Me</Link>
+          <Link to="/about" className="text-gray-700 dark:text-gray-200 hover:text-gray-200 dark:hover:text-white transition duration-300 ease-in-out hover:scale-105 ">About Me</Link>
 
-          <Link to="/" className="text-gray-700 dark:text-gray-200 hover:text-gray-200 dark:hover:text-white">My Skills/Stack</Link>
+          <Link to="/" className="text-gray-700 dark:text-gray-200 hover:text-gray-200 dark:hover:text-white transition duration-200 ease-in-out hover:scale-105">My Skills/Stack</Link>
 
-          <Link to="/contact" className="text-gray-700 dark:text-gray-200 hover:text-gray-200 dark:hover:text-white">Contact Me!</Link>
+          <Link to="/contact" className="text-gray-700 dark:text-gray-200 hover:text-gray-200 dark:hover:text-white transition duration-200 ease-in-out hover:scale-105">Contact Me</Link>
 
           
 
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -7,39 +7,10 @@ export default function Contact() {
 
         <h1 className="mb-3 text-2xl font-semibold text-center text-gray-900 dark:text-white">Contact</h1>
 
-        <p className="mb-6 text-center text-gray-700 dark:text-gray-300">Leave a message and I’ll get back to you soon.</p>
-
-        <form>
-
-          <div>
-            <label htmlFor="email" className="block mb-1 text-sm text-gray-700 dark:text-gray-300">Email</label>
-            <input id="email" name="email" type="email"
-              className="w-full p-3 text-sm bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:text-white"
-              placeholder="example@example.example" />
-          </div>
-
-          <div>
-            <label htmlFor="subject" className="block mb-1 text-sm text-gray-700 dark:text-gray-300">Subject</label>
-            <input id="subject" name="subject" type="text"
-              className="w-full p-3 text-sm bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:text-white"
-              placeholder="Subject" />
-          </div>
-
-          <div>
-            <label htmlFor="message" className="block mb-1 text-sm text-gray-700 dark:text-gray-300">Message</label>
-            <textarea id="message" name="message" rows={6}
-              className="w-full p-3 text-sm bg-gray-50 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:text-white"
-              placeholder="Anything :3" />
-          </div>
-
-          <div>
-            <button type="submit"
-              className="inline-flex items-center justify-center gap-2 py-3 px-5 text-sm font-medium text-white bg-indigo-600 rounded-md hover:bg-indigo-700">
-              Send
-            </button>
-          </div>
-
-        </form>
+        <p className="mb-6 text-center text-gray-700 dark:text-gray-300">To get in contact with me dont hesitate to send an email to : </p>
+        <a href="mailto:anasagrich24@gmail.com">
+        <p className='text-blue-800 dark:text-blue-500  flex justify-center'> anasagrich24@gmail.com </p> 
+        </a>
 
       </div>
     </section>
